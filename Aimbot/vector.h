bad24140ce9caf90fb1c9e00a28dacf388c72073
Ahmad Faisal Mirza <62896098+ahmadfaisal9900@users.noreply.github.com>
2024-11-28 02:09:34 +0500

    CSGO Aimbot

---
+++ Aimbot/vector.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <numbers>
+#include <cmath>
+#define _USE_MATH_DEFINES
+# define M_PI           3.14159265358979323846f  /* pi */
+struct Vector3
+{
+	// constructor
+	constexpr Vector3(
+		const float x = 0.f,
+		const float y = 0.f,
+		const float z = 0.f) noexcept :
+		x(x), y(y), z(z) { }
+
+	// operator overloads
+	constexpr const Vector3& operator-(const Vector3& other) const noexcept
+	{
+		return Vector3{ x - other.x, y - other.y, z - other.z };
+	}
+
+	constexpr const Vector3& operator+(const Vector3& other) const noexcept
+	{
+		return Vector3{ x + other.x, y + other.y, z + other.z };
+	}
+
+	constexpr const Vector3& operator/(const float factor) const noexcept
+	{
+		return Vector3{ x / factor, y / factor, z / factor };
+	}
+
+	constexpr const Vector3& operator*(const float factor) const noexcept
+	{
+		return Vector3{ x * factor, y * factor, z * factor };
+	}
+
+	// utils
+	constexpr const Vector3& ToAngle() const noexcept
+	{
+		return Vector3{
+			std::atan2(-z, std::hypot(x, y)) * (180.0f / M_PI),
+			std::atan2(y, x) * (180.0f / M_PI),
+			0.0f };
+	}
+
+	constexpr const bool IsZero() const noexcept
+	{
+		return x == 0.f && y == 0.f && z == 0.f;
+	}
+
+	// struct data
+	float x, y, z;
+};
